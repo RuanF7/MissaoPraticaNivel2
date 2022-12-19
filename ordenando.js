88333fe7a885c1f689e1f4f12abeeee7422f08c1
Ruan Finamor <75641084+RuanF7@users.noreply.github.com>
2022-12-19 09:33:45 -0300

    Commit Inicial

---
+++ ordenando.js
@@ -0,0 +1,36 @@
+swap = () => {
+
+}
+
+shuffle = () => {
+    
+}
+
+bubble_sort = () => {
+    
+}
+
+selection_sort = () => {
+    
+}
+
+quick_sort = () => {
+    
+}
+
+particionamento = () => {
+    
+}
+
+function add() {
+    let valor = document.getElementById("valor");
+    let valores = document.getElementById("valores");
+}
+
+function ordenar() {
+    
+}
+
+function misturar() {
+    
+}

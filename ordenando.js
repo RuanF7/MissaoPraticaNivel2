81a956edf6d72193daff9e12d6db61e5627a48f5
Ruan Finamor <75641084+RuanF7@users.noreply.github.com>
2023-01-11 16:35:25 -0300

    Atualizando funções

--- ordenando.js
+++ ordenando.js
@@ -1,26 +1,60 @@
-swap = () => {
-
+swap = ([a],[b]) => {
+    let a = b;
+    let b = a;
 }
 
-shuffle = () => {
+shuffle = (valor) => {
+    for (let i = valor.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = valor[i];
+        valor[i] = valor[j];
+        valor[j] = temp;
+    }
+
+    return valor;
     
 }
 
-bubble_sort = () => {
-    
+bubble_sort = (valor) => {
+   const val = valor.slice();
+   for (let i = 0; i < val.length - 1; i++) {
+     for (let j = 0; j < val.length - 1 - i; j++) {
+       if (val[j] > val[j+i]) {
+        [val[j], val[j+1]] = [val[j+1], val[j]];
+       }
+     }
+   }
+   return val;
 }
 
-selection_sort = () => {
+selection_sort = (valor) => {
+    const val = valor.slice();
     
+    for (let i = 0; i < val.length - 1; i++ ) {
+        let minIndex = i;
+        for (let j = i + 1; j < val.length; j++) {
+            if (val[j] < val[minIndex]) {
+                minIndex = j;
+            }
+        }
+        [val[i], val[minIndex]] = [val[minIndex], val[i]];
+    }
+
+    return val;
 }
 
-quick_sort = () => {
-    
-}
-
-particionamento = () => {
-    
-}
+quick_sort = (valor) => {
+    if (valor.length <= 1) {
+        return valor;
+    }
+    const pivot = valor[valor.length - 1];
+    const leftVal = [];
+    const rightVal = [];
+    for (const el of valor.slice(0, valor.length - 1)) {
+        el < pivot ? leftVal.push(el) : rightVal.push(el);
+    }
+    return [...quick_sort(leftVal), pivot, ...quick_sort(rightVal)];
+}    
 
 function add() {
     let valor = document.getElementById("valor");
